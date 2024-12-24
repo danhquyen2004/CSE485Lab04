0ffe084c6bae702a73555f6940a0f47710a9fc00
quyendanh04 <quyendanh04@gmail.com>
2024-12-24 17:27:56 +0700

    fix

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -22,6 +22,7 @@
             <thead class="table-light">
                 <tr>
                     <th>ID</th>
+                    <th>Name</th>
                     <th>Author</th>
                     <th>Category</th>
                     <th>Year</th>
@@ -33,6 +34,7 @@
                 @foreach ($books as $book)
                     <tr>
                         <td>{{ $book->id }}</td>
+                        <td>{{ $book->name }}</td>
                         <td>{{ $book->author }}</td>
                         <td>{{ $book->category }}</td>
                         <td>{{ $book->year }}</td>
